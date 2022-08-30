e3e3f64ab11c0a1093612aa44eb32bd7b4c97a4c
Ömer Faruk DİDİN <fdidin59@gmail.com>
2022-08-30 21:02:36 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+Console.WriteLine("Hello, World!");
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+foreach (var product in productManager.GetAll())
+{
+    Console.WriteLine(product.ProductName);
+}
+
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,18 @@
+﻿using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        List<Product> GetAllByCategoryId(int categoryId);
+    }
+}
