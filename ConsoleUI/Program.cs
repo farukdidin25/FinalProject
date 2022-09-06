35322ef09912b6b4ad0480addd4a36acdcb4ab7e
Ömer Faruk DİDİN <fdidin59@gmail.com>
2022-09-06 20:11:32 +0300

    Entity Framework

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Business.Concrete;
+using DataAccess.Concrete.EntiityFramework;
 using DataAccess.Concrete.InMemory;
 
 Console.WriteLine("Hello, World!");
 
-ProductManager productManager = new ProductManager(new InMemoryProductDal());
+ProductManager productManager = new ProductManager(new EfProductDal());
 foreach (var product in productManager.GetAll())
 {
     Console.WriteLine(product.ProductName);
---
+++ DataAccess/Concrete/EntiityFramework/NortwindContext.cs
@@ -0,0 +1,23 @@
+﻿using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntiityFramework
+{
+    public class NortwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;DataBase=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
