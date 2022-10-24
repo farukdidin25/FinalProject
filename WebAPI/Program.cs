27c2546f11e1d38128d288ae158f05a74b4bdf44
Ömer Faruk DİDİN <fdidin59@gmail.com>
2022-10-24 15:45:57 +0300

    Autofac & AOP

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,15 +1,22 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntiityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+builder.RegisterModule(new AutofacBusinessModule()));
+
+
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IProductService, ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
+
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
